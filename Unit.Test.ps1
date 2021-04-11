5246fc8ce345714d72359817f6a9d76eed185274
fyhao <fyhao1@gmail.com>
2021-04-11 21:50:38 +0800

    update

--- Unit.Test.ps1
+++ Unit.Test.ps1
@@ -1,4 +1,22 @@
-. "$PSScriptRoot\HelloWorld.example.ps1"
+function Get-HelloWorld {
+	<#
+    .SYNOPSIS
+    Outputs "Hello World"
+    
+    .DESCRIPTION
+    Output "Hello World" or "Hello <Name>" if Name is supplied to the CmdLet.
+    
+    .PARAMETER person
+    Name of the person to display. If not supplied, Hello World should be displayed
+    
+    .EXAMPLE
+    Get-HelloWorld
+    #>
+	param( [string]$person = "World")
+
+	return "Hello, $person!"
+}
+
 Describe "HelloWorldTest" {
 	It "Outputs: 'Hello, World!'" {
 		Get-HelloWorld | Should be 'Hello, World!'
