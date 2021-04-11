07b593fa13c525f30e882cd3ad3cd58cedf6e727
fyhao <fyhao1@gmail.com>
2021-04-11 22:01:09 +0800

    MakeHTTPCall

--- Unit.Test.ps1
+++ Unit.Test.ps1
@@ -13,10 +13,23 @@ BeforeAll {
 
         $planets | Where-Object { $_.Name -like $Name }
     }
+	
+	function MakeHTTPCall {
+		$Response = Invoke-WebRequest -URI https://www.bing.com/search?q=how+many+feet+in+a+mile
+		$Response.InputFields | Where-Object {
+			$_.name -like "* Value*"
+		} | Select-Object Name, Value
+	}
 }
+ 
 Describe 'Get-Planet' {
     It 'Given no parameters, it lists all 8 planets' {
         $allPlanets = Get-Planet
         $allPlanets.Count | Should -Be 8
     }
+	
+	It 'Make HTTP Call' {
+		MakeHTTPCall
+    }
 }
+
